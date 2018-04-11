94a78131ddf87076c5480db1ef968276bf4a6897
diarmuidwhelan <36341007+diarmuidwhelan@users.noreply.github.com>
2018-04-11 22:47:22 +0100

    Update dataset_initial_exploration.py

--- dataset_initial_exploration.py
+++ dataset_initial_exploration.py
@@ -6,8 +6,23 @@
 #Iris Dataset initial exploration
 #Will provide summary stats and plots for the Iris Dataset
 ######################################################################################################################
+
 import numpy
-with open('iris.csv') as f:
-    print(f.read())
-    # data=numpy.genfromtxt('iris.csv',delimiter=',')
-print(f)
+import matplotlib.pyplot as pl
+#Open the IRIS.CSV FILE
+iris=numpy.genfromtxt('C:/Users/DELL PC/Documents/GMIT DATA ANALYTICS/Programming and Scripting/iris.csv',delimiter=',')
+print(iris)
+
+#Print the shape of the data
+print(iris.data.shape)
+#Iris data has 150 rows and 5 columns
+
+#Print mean of First 4 columns
+
+for i in range (0,4):
+    column =iris[:,i]
+    meancolumn=numpy.mean(column)
+    print("Mean of column ", i+1," is:",meancolumn)
+#Plot each columns data
+    pl.hist(column)
+    pl.show()
