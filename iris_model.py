abc73ee9340db6ef76cf46e61b2d8ac94d051dae
diarmuidwhelan <36341007+diarmuidwhelan@users.noreply.github.com>
2018-04-22 22:43:49 +0100

    Create iris_model.py

---
+++ iris_model.py
@@ -0,0 +1,12 @@
+###################################################################################################################
+#GMIT Higher Diploma in Data Analytics
+#Scripting and Programming
+#Python Project
+#Diarmuid Whelan, 2018-04-22
+#Iris Dataset predictive model
+#Will attempt to build a predictive model using machine learning techniques for the Iris Dataset
+######################################################################################################################
+import pandas
+import matplotlib.pyplot as plt
+import scipy
+
