0c1ccac9bf3e352bd36c9f0710e50d8a59b6fa43
diarmuidwhelan <36341007+diarmuidwhelan@users.noreply.github.com>
2018-04-23 22:53:10 +0100

    Update iris_model.py

--- iris_model.py
+++ iris_model.py
@@ -7,6 +7,23 @@
 #Will attempt to build a predictive model using machine learning techniques for the Iris Dataset
 ######################################################################################################################
 import pandas
+from sklearn import model_selection
 import matplotlib.pyplot as plt
-import scipy
+#Load Data
+iris = pandas.read_csv('C:/Users/DELL PC/Documents/GMIT DATA ANALYTICS/Programming and Scripting/iris_data.csv')
+#Need to split the data into a test and training set. Will split it 80% training and 20% test
+array = iris.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.2
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
+#FIT KNN
+ 
+#FIT SVM (Support Vector Machine)
+from sklearn  import svm 
+svc = svm.SVC(kernel='linear') # Support Vector Classifier
+svc.fit(X,Y)
+
+#FIT K-MEANS
